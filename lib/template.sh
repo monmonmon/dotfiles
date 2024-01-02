05cc775ac03e94f2afdb7e7aadec73207668a5cf
Yamada Simon <ymdsmn@gmail.com>
2024-01-02 18:03:23 +0900

    lib/template.sh

--- lib/template.sh
+++ lib/template.sh
@@ -1,13 +1,31 @@
 #!/usr/bin/env bash
 PATH=/sbin:/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin
-set -e
+set -euxCo pipefail
+cd "$(dirname "$0")"
 
-if [ $# -ne 1 ]; then
-    echo "Usage: $(basename $0) fname"
-    exit 1
-elif [ ! -f "$1" ]; then
-    echo $1: No such file
-    exit 1
-fi
+function usage() {
+    echo "Usage: $(basename "$0")"
+    exit 0
+}
 
-fname=$1
+# if [ $# -ne 1 ]; then
+#     echo "Usage: $(basename $0) fname"
+#     exit 1
+# elif [ ! -f "$1" ]; then
+#     echo $1: No such file
+#     exit 1
+# fi
+# fname=$1
+
+while getopts "t:h" opt; do
+    case $opt in
+        t)  # tag
+            test -z "$OPTARG" && exit 1;
+            tag=$OPTARG
+            ;;
+        h | *)
+            usage
+            ;;
+    esac
+done
+shift $(($OPTIND - 1))
