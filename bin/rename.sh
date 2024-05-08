f91e1b0050c93e7f566fde219a45c6bb5097a0d3
Yamada Simon <ymdsmn@gmail.com>
2024-05-08 20:47:11 +0900

    rename.sh

--- bin/rename.sh
+++ bin/rename.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/env bash
 
-echo $#
-if [ $# -eq 0 ]; then
-    echo "Usage: ${basename $0} <files to rename>"
-    exit
-fi
-# test -n "$1" -a -d "$1" || exit 1
-
-cd "$1"
 i=1
 for f in $(ls -tr); do
-#for f in $(ls); do
-    j=$(printf "%03d" $i)
-    echo mv $f $j.jpg
-    mv $f $j.jpg
+    if [ ! -f "$f" ]; then
+        continue
+    fi
+    num=$(printf "%03d" $i)
+    ext="${f##*.}"
+    g="$num.$ext"
+    if [ "$f" != "$g" ]; then
+        echo mv "$f" "$g"
+        mv "$f" "$g"
+    fi
     i=$(($i + 1))
 done
