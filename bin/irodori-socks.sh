8feb0a0ca801c41af1af3825dbb797f0665058fc
Yamada Simon <ymdsmn@gmail.com>
2024-06-03 13:07:40 +0900

    add bin/irodori-socks.sh

---
+++ bin/irodori-socks.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+show_help() {
+	cat 1>&2 << EOS
+Usage: $0 [ -u ssh_username ]
+	-u: ssh username. If the username is same as local, this option is not needed.
+EOS
+	exit 1
+}
+
+while getopts "u:h" OPT
+do
+	case $OPT in
+		u) ssh_user=$OPTARG ;;
+		h) show_help ;;
+		?) show_help ;;
+	esac
+done
+
+ssh_user=ymdsmn
+
+port_check() {
+    netstat -an|grep LISTEN|grep "127.0.0.1.${1}"|wc -l
+}
+
+is_using_port_45522=$(port_check 45522)
+if [ $is_using_port_45522 -eq 0 ]; then
+    ssh -N -f -L 45522:10.2.110.55:22 ${ssh_user}@59.158.57.249
+fi
+
+is_using_port_48080=$(port_check 48080)
+if [ $is_using_port_48080 -ne 0 ]; then
+	echo 'Port 48080 is already used' >&2
+	echo 'Perhaps already socks proxy is working' >&2
+	exit 1
+fi
+
+ssh -oHostKeyAlias=10.2.110.55 ${ssh_user}@127.0.0.1 -p45522 -D 48080 -N -f
