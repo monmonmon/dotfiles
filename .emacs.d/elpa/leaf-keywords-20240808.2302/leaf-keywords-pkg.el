00c193d16a35b6ffe02a1e2c885a2bc01e17c7ba
Yamada Simon <ymdsmn@gmail.com>
2024-08-20 01:19:12 +0900

    .emacs.d/elpa

---
+++ .emacs.d/elpa/leaf-keywords-20240808.2302/leaf-keywords-pkg.el
@@ -0,0 +1,15 @@
+(define-package "leaf-keywords" "20240808.2302" "Additional leaf.el keywords for external packages"
+  '((emacs "24.4")
+    (leaf "3.5.0"))
+  :commit "82ec27e3441900daedeaaebca509181f964da81f" :authors
+  '(("Naoya Yamashita" . "conao3@gmail.com"))
+  :maintainers
+  '(("Naoya Yamashita" . "conao3@gmail.com"))
+  :maintainer
+  '("Naoya Yamashita" . "conao3@gmail.com")
+  :keywords
+  '("lisp" "settings")
+  :url "https://github.com/conao3/leaf-keywords.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
